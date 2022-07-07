b99e2752325e41642f00e962562eaf4d2216d06e
mwafrika <25848053+mwafrika@users.noreply.github.com>
2022-07-07 21:52:01 +0300

    Handle errors

--- app/models/transanct.rb
+++ app/models/transanct.rb
@@ -2,7 +2,7 @@ class Transanct < ApplicationRecord
   belongs_to :user, class_name: 'User'
   has_many :category_transacts, dependent: :destroy
   has_many :categories, through: :category_transacts, dependent: :destroy
-  validates_presence_of :name, :amount, :user
+  validates_presence_of :user
+  validates :name, presence: true, length: { maximum: 30 }
+  validates :amount, presence: true
 end
-
-# checked
